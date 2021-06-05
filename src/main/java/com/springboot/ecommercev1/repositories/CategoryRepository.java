d9c9c52fb33e3277b9a8e12dfe10c15362ad54a0
kristiancruz16 <kristian_m_cruz12@yahoo.com>
2021-06-05 16:29:33 +0800

    Create Filter Search in CategoryRepository. Closes #12

--- src/main/java/com/springboot/ecommercev1/repositories/CategoryRepository.java
+++ src/main/java/com/springboot/ecommercev1/repositories/CategoryRepository.java
@@ -3,9 +3,15 @@ package com.springboot.ecommercev1.repositories;
 import com.springboot.ecommercev1.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Set;
+
 /**
  * @author KMCruz
  * 6/5/2021
  */
 public interface CategoryRepository extends JpaRepository<Category,Long> {
+    Category findByCategoryCode(String categoryCode);
+
+    Set<Category> findAllByNameLikeIgnoreCase(String name);
+
 }
