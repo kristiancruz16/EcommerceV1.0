930a4a83b2a2124c499e7c30eb2721d01b723f39
kristiancruz16 <kristian_m_cruz12@yahoo.com>
2021-06-05 15:36:40 +0800

    Create Relationship for Product and Category. Closes #10

--- src/main/java/com/springboot/ecommercev1/domain/Category.java
+++ src/main/java/com/springboot/ecommercev1/domain/Category.java
@@ -5,7 +5,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.Set;
 
 /**
  * @author KMCruz
@@ -16,12 +20,18 @@ import javax.persistence.Entity;
 @Setter
 @NoArgsConstructor
 @Entity
+@Table(name = "category")
 public class Category extends BaseEntity{
 
     @Builder
-    public Category(Long id, String name) {
+    public Category(Long id, String name, String categoryCode, Set<Product> products) {
         super(id, name);
+        this.categoryCode = categoryCode;
+        this.products = products;
     }
 
     private String categoryCode;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+    private Set<Product> products;
 }
--- src/main/java/com/springboot/ecommercev1/domain/Product.java
+++ src/main/java/com/springboot/ecommercev1/domain/Product.java
@@ -3,6 +3,9 @@ package com.springboot.ecommercev1.domain;
 import lombok.*;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 /**
  * @author KMCruz
@@ -13,15 +16,23 @@ import javax.persistence.Entity;
 @Setter
 @NoArgsConstructor
 @Entity
+@Table(name = "product")
 public class Product extends BaseEntity{
 
-    @Builder
-    public Product(Long id, String name) {
+    public Product(Long id, String name, Long sku, String productDescription, Integer productPrice, Category category) {
         super(id, name);
+        this.sku = sku;
+        this.productDescription = productDescription;
+        this.productPrice = productPrice;
+        this.category = category;
     }
 
     private Long sku;
     private String productDescription;
     private Integer productPrice;
+
+    @ManyToOne
+    @JoinColumn("product_id")
+    private Category category;
 
 }
