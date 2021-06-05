c1090b8bd88fef0e69a854c3ba7f2e3a498918fc
kristiancruz16 <kristian_m_cruz12@yahoo.com>
2021-06-05 14:08:16 +0800

    Initial commit

---
+++ src/main/java/com/springboot/ecommercev1/Ecommercev1Application.java
@@ -0,0 +1,13 @@
+package com.springboot.ecommercev1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ecommercev1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ecommercev1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/ecommercev1/Ecommercev1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.ecommercev1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ecommercev1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
