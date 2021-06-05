cc1f597c8f7c34ae943d39223ff431451670ece3
kristiancruz16 <kristian_m_cruz12@yahoo.com>
2021-06-05 19:05:44 +0800

    Create CrudService Interface with Basic CRUD, Closes #14

---
+++ src/main/java/com/springboot/ecommercev1/services/ProductService.java
@@ -0,0 +1,16 @@
+package com.springboot.ecommercev1.services;
+
+import com.springboot.ecommercev1.domain.Product;
+
+import java.util.Set;
+
+/**
+ * @author KMCruz
+ * 6/5/2021
+ */
+public interface ProductService extends CrudService<Product,Long>{
+    
+    Set<Product> findAllBySkuLikeIgnoreCase(Long sku);
+
+    Set<Product> findAllByNameLikeIgnoreCase(String name);
+}
