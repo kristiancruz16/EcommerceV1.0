74a06b289a1588ceda125b4b4c26495665197c78
kristiancruz16 <kristian_m_cruz12@yahoo.com>
2021-06-05 19:18:13 +0800

    Implement Category Service. Closes #18

---
+++ src/main/java/com/springboot/ecommercev1/services/CategoryServiceImpl.java
@@ -0,0 +1,46 @@
+package com.springboot.ecommercev1.services;
+
+import com.springboot.ecommercev1.domain.Category;
+
+import java.util.Set;
+
+/**
+ * @author KMCruz
+ * 6/5/2021
+ */
+public class CategoryServiceImpl implements CategoryService {
+    @Override
+    public Category findByCategoryCode(String categoryCode) {
+        return null;
+    }
+
+    @Override
+    public Set<Category> findAllByNameLikeIgnoreCase(String name) {
+        return null;
+    }
+
+    @Override
+    public Set<Category> findAll() {
+        return null;
+    }
+
+    @Override
+    public Category findById(Long aLong) {
+        return null;
+    }
+
+    @Override
+    public Category save(Category object) {
+        return null;
+    }
+
+    @Override
+    public void delete(Category object) {
+
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+
+    }
+}
