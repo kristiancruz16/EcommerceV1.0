1c6a7f374ca16592016e16125fb8e07808b0d54c
kristiancruz16 <kristian_m_cruz12@yahoo.com>
2021-06-05 15:20:12 +0800

    Create Controller for Product. Closes #7

---
+++ src/main/java/com/springboot/ecommercev1/controller/ProductController.java
@@ -0,0 +1,20 @@
+package com.springboot.ecommercev1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author KMCruz
+ * 6/5/2021
+ */
+@Controller
+@RequestMapping("/product")
+public class ProductController {
+
+    @GetMapping
+    public String showAllProducts(){
+        return "/products/findProduct";
+    }
+
+}
