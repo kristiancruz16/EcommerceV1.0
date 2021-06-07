df2e78d98c63e4c8d0cf6d70dbade669696d6173
kristiancruz16 <kristian_m_cruz12@yahoo.com>
2021-06-07 16:30:36 +0800

    Added a method to Category Controller that will display the specific category that client requested. Closes #36

--- src/main/java/com/springboot/ecommercev1/controllers/CategoryController.java
+++ src/main/java/com/springboot/ecommercev1/controllers/CategoryController.java
@@ -5,6 +5,7 @@ import com.springboot.ecommercev1.services.CategoryService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -25,5 +26,11 @@ public class CategoryController {
     public String showAllCategory(Model model){
         model.addAttribute("categories",categoryService.findAll());
         return "/categories/showAllCategory";
+    }
+
+    @GetMapping("/{categoryId}")
+    public String showCategoryDetails (@PathVariable Long categoryId, Model model) {
+        model.addAttribute("category",categoryService.findById(categoryId));
+        return "/categories/categoryDetails";
     }
 }
