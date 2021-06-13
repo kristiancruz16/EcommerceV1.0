a38b4366e8d2c7b33ad2d52993ea1865785c5b7e
kristiancruz16 <kristian_m_cruz12@yahoo.com>
2021-06-13 13:30:02 +0800

    Created ShoppingStore Controller Page. Closes #86

---
+++ src/main/java/com/springboot/ecommercev1/controllers/ShoppingStoreController.java
@@ -0,0 +1,16 @@
+package com.springboot.ecommercev1.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author KMCruz
+ * 6/13/2021
+ */
+
+@Controller
+@RequestMapping
+public class ShoppingStoreController {
+
+    
+}
