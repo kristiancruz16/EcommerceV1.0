b1145ec19c7a18aee79e7ee1836236c315202625
kristiancruz16 <kristian_m_cruz12@yahoo.com>
2021-06-13 13:51:34 +0800

    Created mapping for default Page of the Shopping Store. Closes #87

--- src/main/java/com/springboot/ecommercev1/controllers/ShoppingStoreController.java
+++ src/main/java/com/springboot/ecommercev1/controllers/ShoppingStoreController.java
@@ -1,6 +1,10 @@
 package com.springboot.ecommercev1.controllers;
 
+import com.springboot.ecommercev1.services.CategoryService;
+import com.springboot.ecommercev1.services.ProductService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -12,5 +16,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping
 public class ShoppingStoreController {
 
-    
+    private final CategoryService categoryService;
+    private final ProductService productService;
+
+    public ShoppingStoreController(CategoryService categoryService, ProductService productService) {
+        this.categoryService = categoryService;
+        this.productService = productService;
+    }
+
+    @GetMapping({"","/home","/"})
+    public String displayHomePage (Model model) {
+        model.addAttribute("categories",categoryService.findAll());
+        return "shoppingStore/homePage";
+    }
 }
