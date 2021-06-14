b83649e9ef947d8adda7f95ec4f2e3e33a3d60cf
kristiancruz16 <kristian_m_cruz12@yahoo.com>
2021-06-14 17:04:51 +0800

    Implemented Service Class of Shopping Cart Line Items. Closes #104

---
+++ src/main/java/com/springboot/ecommercev1/services/ShoppingCartLineItemServiceImpl.java
@@ -0,0 +1,37 @@
+package com.springboot.ecommercev1.services;
+
+import com.springboot.ecommercev1.domain.ShoppingCartLineItem;
+import com.springboot.ecommercev1.domain.ShoppingCartLineItemKey;
+
+import java.util.List;
+
+/**
+ * @author KMCruz
+ * 6/14/2021
+ */
+public class ShoppingCartLineItemServiceImpl implements ShoppingCartLineItemService {
+    @Override
+    public List<ShoppingCartLineItem> findAll() {
+        return null;
+    }
+
+    @Override
+    public ShoppingCartLineItem findById(ShoppingCartLineItemKey shoppingCartLineItemKey) {
+        return null;
+    }
+
+    @Override
+    public ShoppingCartLineItem save(ShoppingCartLineItem object) {
+        return null;
+    }
+
+    @Override
+    public void delete(ShoppingCartLineItem object) {
+
+    }
+
+    @Override
+    public void deleteById(ShoppingCartLineItemKey shoppingCartLineItemKey) {
+
+    }
+}
