9bc6b666bf5891b96628436167a3088a27f8dee6
kristiancruz16 <kristian_m_cruz12@yahoo.com>
2021-06-14 17:00:29 +0800

    Created a Service Class for Shopping Cart. Closes #101

---
+++ src/main/java/com/springboot/ecommercev1/services/ShoppingCartService.java
@@ -0,0 +1,11 @@
+package com.springboot.ecommercev1.services;
+
+import com.springboot.ecommercev1.domain.ShoppingCart;
+
+/**
+ * @author KMCruz
+ * 6/14/2021
+ */
+public interface ShoppingCartService extends CrudService<ShoppingCart,Long> {
+
+}
