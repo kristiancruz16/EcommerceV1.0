07e80287e7d889b4031b8ca9513edfdd09e7c515
kristiancruz16 <kristian_m_cruz12@yahoo.com>
2021-06-14 17:01:38 +0800

    Implemented Service Class of Shopping Cart. Closes #103

---
+++ src/main/java/com/springboot/ecommercev1/services/ShoppingCartServiceImpl.java
@@ -0,0 +1,36 @@
+package com.springboot.ecommercev1.services;
+
+import com.springboot.ecommercev1.domain.ShoppingCart;
+
+import java.util.List;
+
+/**
+ * @author KMCruz
+ * 6/14/2021
+ */
+public class ShoppingCartServiceImpl implements ShoppingCartService {
+    @Override
+    public List<ShoppingCart> findAll() {
+        return null;
+    }
+
+    @Override
+    public ShoppingCart findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public ShoppingCart save(ShoppingCart shoppingCart) {
+        return null;
+    }
+
+    @Override
+    public void delete(ShoppingCart shoppingCart) {
+
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+    }
+}
