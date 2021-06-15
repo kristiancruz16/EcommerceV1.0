44b56e9c7c0005add1b55526ca7d619228891177
kristiancruz16 <kristian_m_cruz12@yahoo.com>
2021-06-15 09:42:07 +0800

    Improved Implementation of Shopping Cart Service. Closes #109

--- src/main/java/com/springboot/ecommercev1/services/ShoppingCartServiceImpl.java
+++ src/main/java/com/springboot/ecommercev1/services/ShoppingCartServiceImpl.java
@@ -1,36 +1,51 @@
 package com.springboot.ecommercev1.services;
 
 import com.springboot.ecommercev1.domain.ShoppingCart;
+import com.springboot.ecommercev1.repositories.ShoppingCartRepository;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * @author KMCruz
  * 6/14/2021
  */
+@Service
 public class ShoppingCartServiceImpl implements ShoppingCartService {
+
+    private final ShoppingCartRepository shoppingCartRepository;
+
+    public ShoppingCartServiceImpl(ShoppingCartRepository shoppingCartRepository) {
+        this.shoppingCartRepository = shoppingCartRepository;
+    }
+
     @Override
     public List<ShoppingCart> findAll() {
-        return null;
+        List <ShoppingCart> shoppingCartList = new ArrayList<>();
+        shoppingCartRepository.findAll().forEach(shoppingCartList::add);
+        return shoppingCartList;
     }
 
     @Override
     public ShoppingCart findById(Long id) {
-        return null;
+
+        return shoppingCartRepository.findById(id).orElse(null);
     }
 
     @Override
     public ShoppingCart save(ShoppingCart shoppingCart) {
-        return null;
+
+        return shoppingCartRepository.save(shoppingCart);
     }
 
     @Override
     public void delete(ShoppingCart shoppingCart) {
-
+        shoppingCartRepository.delete(shoppingCart);
     }
 
     @Override
     public void deleteById(Long id) {
-
+        shoppingCartRepository.deleteById(id);
     }
 }
