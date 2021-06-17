3ab4bed880130614cb90eb4be7371a27de3a742b
kristiancruz16 <kristian_m_cruz12@yahoo.com>
2021-06-17 15:59:18 +0800

    Created a Shopping Cart Controller and a method that will display the Shopping Cart View. Closes #123

---
+++ src/main/java/com/springboot/ecommercev1/controllers/ShoppingCartController.java
@@ -0,0 +1,31 @@
+package com.springboot.ecommercev1.controllers;
+
+import com.springboot.ecommercev1.domain.ShoppingCart;
+import com.springboot.ecommercev1.services.ShoppingCartService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * @author KMCruz
+ * 6/17/2021
+ */
+@Controller
+@RequestMapping("/shoppingcart")
+public class ShoppingCartController {
+    private final ShoppingCartService shoppingCartService;
+
+    public ShoppingCartController(ShoppingCartService shoppingCartService) {
+        this.shoppingCartService = shoppingCartService;
+    }
+
+    @GetMapping
+    public String showShoppingCart(HttpSession session, Model model) {
+        ShoppingCart shoppingCart = shoppingCartService.findById(session.getId());
+        model.addAttribute("shoppingCart", shoppingCart);
+        return "store/shoppingCart";
+    }
+}
