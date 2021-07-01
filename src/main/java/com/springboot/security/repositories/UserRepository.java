ac6902420f56231490a51150256eec55c954f986
kristiancruz16 <kristian_m_cruz12@yahoo.com>
2021-07-01 16:05:27 +0800

    Create a Repository for User. Closes #134

---
+++ src/main/java/com/springboot/security/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.springboot.security.repositories;
+
+import com.springboot.security.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author KMCruz
+ * 7/1/2021
+ */
+public interface UserRepository extends JpaRepository<User,Long> {
+}
