fe03841b08b3963cb1ea67331a547cd56943930e
kristiancruz16 <kristian_m_cruz12@yahoo.com>
2021-07-04 11:42:14 +0800

    Create scheduled task that deletes unverified email address on a specific time daily. Closes #145

--- src/main/java/com/springboot/security/repositories/VerificationTokenRepository.java
+++ src/main/java/com/springboot/security/repositories/VerificationTokenRepository.java
@@ -4,6 +4,9 @@ import com.springboot.security.models.User;
 import com.springboot.security.models.VerificationToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
+import java.util.List;
+
 /**
  * @author KMCruz
  * 7/2/2021
@@ -12,4 +15,6 @@ public interface VerificationTokenRepository extends JpaRepository<VerificationT
     VerificationToken findVerificationTokenByRegistrationToken(String token);
 
     VerificationToken findVerificationTokenByUser(User user);
+
+    List<VerificationToken> findAllByExpiryDateIsLessThanEqual(Date now);
 }
---
+++ src/main/java/com/springboot/security/tasks/TokenPurgeTask.java
@@ -0,0 +1,49 @@
+package com.springboot.security.tasks;
+
+import com.springboot.security.models.User;
+import com.springboot.security.models.VerificationToken;
+import com.springboot.security.repositories.UserRepository;
+import com.springboot.security.repositories.VerificationTokenRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.time.Instant;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author KMCruz
+ * 7/4/2021
+ */
+@Service
+@Transactional
+public class TokenPurgeTask {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(getClass());
+
+    private final VerificationTokenRepository verificationTokenRepository;
+    private final UserRepository userRepository;
+
+    public TokenPurgeTask(VerificationTokenRepository verificationTokenRepository, UserRepository userRepository) {
+        this.verificationTokenRepository = verificationTokenRepository;
+        this.userRepository = userRepository;
+    }
+
+    @Scheduled(cron = "${cron.scheduled.time}" )
+    public void purgeToken(){
+        LOGGER.info("Purging Expired Verification Tokens");
+
+        Date now = Date.from(Instant.now());
+        List<VerificationToken> tokenList = verificationTokenRepository.findAllByExpiryDateIsLessThanEqual(now);
+        tokenList.stream().forEach(
+                token->{
+                    User user = token.getUser();
+                    verificationTokenRepository.delete(token);
+                    userRepository.delete(user);
+                });
+
+    }
+}
